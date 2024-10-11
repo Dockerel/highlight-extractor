07607048e38dd11fbb5e3c7a55674eb6e40c60a6
Dockerel <dgh0001@naver.com>
2024-10-11 11:11:20 +0900

    feat: cors 설정 추가

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI, HTTPException, BackgroundTasks, Response
+from fastapi.middleware.cors import CORSMiddleware
 from models import SubtitleAdderDto
 from utils import SubtitleAdder
 import uvicorn
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def process_subtitle(dto: SubtitleAdderDto):
